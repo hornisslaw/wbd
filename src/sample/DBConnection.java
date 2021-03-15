e85ce34c1254df27075fdeac1b7ae7f82b921837
hornisslaw <62252188+hornisslaw@users.noreply.github.com>
2021-03-15 10:35:29 +0100

    Update DBConnection.java
    Removed ulr, login and password.

--- src/sample/DBConnection.java
+++ src/sample/DBConnection.java
@@ -6,12 +6,10 @@ import java.sql.*;
 public class DBConnection {
     private static Connection conn;
     public static Connection getConnection()  {
-        String DB_URL = "jdbc:oracle:thin:@localhost:1521:ORCL1"; //ta sciezka jest tak jak na wykladzie
-        String DB_USER = "RKACZMAR3"; //twoj usser nie system
-        String DB_PASS = "amatorobson";  //twoje haslo usera
-        //String DB_URL = "jdbc:oracle:thin:@localhost:1521:Fanso98"; //ta sciezka jest tak jak na wykladzie
-        //String DB_USER = "KHARBASZ"; //twoj usser nie system
-        //String DB_PASS = "hujhujs";  //twoje haslo usera
+        String DB_URL = ""; //ta sciezka jest tak jak na wykladzie
+        String DB_USER = ""; //twoj usser nie system
+        String DB_PASS = "";  //twoje haslo usera
+        
         try{
         conn = DriverManager.getConnection(DB_URL,DB_USER,DB_PASS);
 
